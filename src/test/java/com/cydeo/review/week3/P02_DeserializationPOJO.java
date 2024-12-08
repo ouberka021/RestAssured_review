1eb04612f12acec59cf14b19f2f25deb12572638
taieb <ouberka021@gmail.com>
2024-12-07 21:11:41 -0500

    POJO final test

--- src/test/java/com/cydeo/review/week3/P02_DeserializationPOJO.java
+++ src/test/java/com/cydeo/review/week3/P02_DeserializationPOJO.java
@@ -6,6 +6,8 @@ import com.cydeo.pojo.Product;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -50,44 +52,76 @@ public class P02_DeserializationPOJO extends FakeStoreTestBase {
                 .statusCode(200)
                 .contentType(ContentType.JSON.withCharset("utf-8"))
                 .extract().jsonPath();
-        
-      System.out.println("====== GET FIRST PRODUCTS ======");
-        Product firstProduct = jp.getObject("[0]",Product.class);
-        System.out.println("first Product = " + firstProduct);
 
-      System.out.println("====== GET FIRST IMAGES ======");
-      List<String> images = firstProduct.getAllImages();
-        System.out.println("images = " + images);
+        System.out.println("====== GET FIRST PRODUCTS ======");
+        Product firstProduct = jp.getObject("[0]", Product.class);
+        System.out.println("firstProduct = " + firstProduct);
 
-      System.out.println("====== GET FIRST PRODUCT CATEGORY ======");
-     CategoryPOJO category = firstProduct.getCategory();
+        System.out.println("====== GET FIRST IMAGES ======");
+        List<String> allImages = firstProduct.getAllImages();
+        System.out.println("allImages = " + allImages);
 
-      System.out.println("====== GET ALL PRODUCTS ======");
-      List<Product> allProducts = jp.getList("", Product.class);
-      System.out.println("all Products = " + allProducts);
+        System.out.println("====== GET FIRST PRODUCT CATEGORY ======");
+        CategoryPOJO category = firstProduct.getCategory();
+        System.out.println("category = " + category);
 
-      System.out.println("====== GET SECOND PRODUCTS ======");
-      Product secondProduct = allProducts.get(1);
-      System.out.println("second Product = " + secondProduct);
 
-      System.out.println("====== GET SECOND PRODUCTS PRICE ======");
-      int price = secondProduct.getPrice();
-      System.out.println("price = " + price);
+        System.out.println("====== GET ALL PRODUCTS ======");
+        List<Product> allProducts = jp.getList("", Product.class);
+        System.out.println("allProducts = " + allProducts);
 
-      System.out.println("====== GET SECOND PRODUCTS IMAGES ======");
-      List<String> secondProductImages = secondProduct.getAllImages();
-      System.out.println("secondProductImages = " + secondProductImages);
+        System.out.println("====== GET SECOND PRODUCTS ======");
+        Product secondProduct = allProducts.get(1);
+        System.out.println("secondProduct = " + secondProduct);
 
-      System.out.println("====== GET SECOND PRODUCTS FIRST IMAGE ======");
-      String firstImage = secondProductImages.get(0);
-      System.out.println("firstImage = " + firstImage);
+        System.out.println("====== GET SECOND PRODUCTS PRICE ======");
+        int price = secondProduct.getPrice();
+        System.out.println("price = " + price);
 
-      System.out.println("====== GET SECOND PRODUCTS CATEGORY ======");
-      CategoryPOJO secondCategory = secondProduct.getCategory();
-      System.out.println("====== GET SECOND PRODUCTS CATEGORY NAME ======");
-      String categoryName = secondCategory.getName();
-      System.out.println("categoryName = " + categoryName);
-        
+        System.out.println("====== GET SECOND PRODUCTS IMAGES ======");
+        List<String> secondProductImages = secondProduct.getAllImages();
+        System.out.println("secondProductImages = " + secondProductImages);
+
+
+        System.out.println("====== GET SECOND PRODUCTS FIRST IMAGE ======");
+        String firstImage = secondProductImages.get(0);
+        System.out.println("firstImage = " + firstImage);
+
+        System.out.println("====== GET SECOND PRODUCTS CATEGORY ======");
+        CategoryPOJO secondProductCategory = secondProduct.getCategory();
+        System.out.println("secondProductCategory = " + secondProductCategory);
+
+        System.out.println("====== GET SECOND PRODUCTS CATEGORY NAME ======");
+        String categoryName = secondProductCategory.getName();
+        System.out.println("categoryName = " + categoryName);
+
+
+        //     * Verify
+        //     * each Product has ID
+        //     * each Product has CategoryID
+        //     * each Product category images are not broken
+
+        for (Product eachProduct : allProducts) {
+            //     * each Product has ID
+            int productID = eachProduct.getId();
+            Assertions.assertNotNull(productID);
+
+            //     * each Product has CategoryID
+            int categoryID = eachProduct.getCategory().getId();
+            Assertions.assertNotNull(categoryID);
+
+            //     * each Product category images are not broken
+            String imageURL = eachProduct.getCategory().getImage();
+
+            // int statusCode = RestAssured.get(imageURL).statusCode();
+
+            Response response = RestAssured.get(imageURL);
+            int statusCode = response.statusCode();
+
+            Assertions.assertEquals(200,statusCode);
+
+
+        }
     }
     
 
